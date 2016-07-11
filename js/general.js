684b4d89f0c414e997b1b236afc58c6206eae570
Jian-Kai Wang <s0973078@alumni.ncyu.edu.tw>
2016-07-11 11:28:26 +0800

    Update js/general.js

--- js/general.js
+++ js/general.js
@@ -14,7 +14,7 @@ $(function() {
   $('#listItem').append('<h4>主題選單</h4><ul>');
   for(var i = 0 ; i < allH3.length ; i++) {
     $(allH3[i]).before('<a name="' + 'h3_' + i + '"></a>');
-    $('#listItem').append('<h5><li><a style="clear:both; font-size: 16px;" href="#' + 'h3_' + i + '">' + $(allH3[i]).text() + '</a></li></h5>');
+    $('#listItem').append('<h5><li><a style="clear:both; font-size: 16px;" onclick="javascript: location.href=\'' + "#h3_" + i + '\'">' + $(allH3[i]).text() + '</a></li></h5>'); 
   }
   $('#listItem').append('</ul>');
 });
